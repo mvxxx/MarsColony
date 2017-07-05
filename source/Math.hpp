31b61fed5d469ff84b257159ba832a879ba777b7
mvxxx <yohama8832@o2.pl>
2017-07-06 00:50:59 +0200

    Mouse pos computing in Math.hpp

--- source/Math.hpp
+++ source/Math.hpp
@@ -6,6 +6,9 @@ https://github.com/mvxxx
 #pragma once
 
 #include <SFML/System/Vector2.hpp>
+#include <SFML/Window/Mouse.hpp>
+
+#include "scene/Scene.hpp"
 
 class Math
 {
@@ -18,6 +21,11 @@ public:
 	static sf::Vector2i convertToUnitPosition( const sf::Vector2f& pxPos, const sf::Vector2f& cellDim )
 	{
 		return sf::Vector2i( pxPos.x / cellDim.x, pxPos.y / cellDim.y );
+	}
+
+	static sf::Vector2f mouseWorldPosition( std::shared_ptr<Scene> scene )
+	{
+		return scene->getWindow()->mapPixelToCoords( sf::Mouse::getPosition( *scene->getWindow() ) );
 	}
 protected:
 private:
