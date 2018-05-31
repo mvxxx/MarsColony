1c37bbc9ae9061e2f4d294bb2bd49a8da8550c42
mvxxx <yohama8832@o2.pl>
2018-05-31 09:32:12 +0200

    Removed selection component && moved source into Frame

---
+++ source/frame/Frame.cpp
@@ -0,0 +1,62 @@
+#include "Frame.hpp"
+
+Frame::Frame( std::shared_ptr<Scene> scene )
+  :Visible(false)
+{
+  constexpr int8_t ammountOfCorners = 7; /*it sums to 8*/
+
+  frame.setPrimitiveType(sf::Lines);
+  for ( size_t i = 0; i <= ammountOfCorners; i++ )
+  {
+    frame.append(sf::Vertex(Math::mouseWorldPosition(scene)));
+    frame[i].color = sf::Color::Yellow;
+  }
+}
+
+void Frame::draw( sf::RenderTarget& target, sf::RenderStates states ) const
+{
+	target.draw( frame, states );
+}
+
+void Frame::activateSelection(const sf::Vector2f& coords)
+{
+  this->setPoint(coords, PointType::START);
+}
+
+void Frame::deactivateSelection(const sf::Vector2f& coords)
+{
+  this->setPoint(coords, PointType::END);
+}
+
+inline const sf::VertexArray& Selection::getFrame()
+{
+  return frame;
+}
+
+void Frame::setPoint(const sf::Vector2f& data, const PointType & status)
+{
+  sf::Vector2f& point = status == PointType::START ? start : end;
+  mv::Logger::Log((status == PointType::START ? "Start Point: " : "End Point ") + std::to_string(point.x) + " " + std::to_string(point.y), mv::Logger::STREAM::CONSOLE, mv::Logger::TYPE::INFO);
+  point.x = data.x;
+  point.y = data.y;
+
+  if ( status == PointType::END )
+    configureFrame();
+}
+
+void Frame::configureFrame()
+{
+  frame[0].position = start;
+  frame[1].position = { start.x,end.y };
+  frame[2].position = { start.x,end.y };
+  frame[3].position = end;
+  frame[4].position = end;
+  frame[5].position = { end.x,start.y };
+  frame[6].position = { end.x, start.y };
+  frame[7].position = start;
+  visible = true;
+}
+
+
+
+
--- source/frame/Frame.hpp
+++ source/frame/Frame.hpp
@@ -6,26 +6,41 @@ https://github.com/mvxxx
 #pragma once
 
 #include <sfml/Graphics/VertexArray.hpp>
+#include "features/Visible.hpp"
 #include "ecs/entity/Entity.hpp"
 #include "scene/Scene.hpp"
-#include "ecs/component/Selection.hpp"
 #include "Math.hpp"
 
 /*not finished*/
-class Border : public mv::Entity, public sf::Drawable
+class Frame : public sf::Drawable, public Visible
 {
 	/* ===Objects=== */
 public:
+  enum class PointType
+  {
+    START,
+    END
+  };
+
 protected:
 private:
+  sf::Vector2f start;
+  sf::Vector2f end;
+
+  sf::VertexArray frame;
 	/* ===Methods=== */
 public:
-	Border( std::shared_ptr<Scene> scene );
+	Frame( std::shared_ptr<Scene> scene );
 
   void activateSelection(const sf::Vector2f& coords);
   void deactivateSelection(const sf::Vector2f& coords);
+
+  const sf::VertexArray& getFrame();
+
+  void setPoint(const sf::Vector2f& coords, const PointType& status);
 protected:
 private:
+  void configureFrame();
 	virtual void draw( sf::RenderTarget& target, sf::RenderStates states ) const;
 };
 
