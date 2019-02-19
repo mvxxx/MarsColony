b62b7d4e82749fb434c477338af5e9a22ed24f97
mvxxx <michal.tyrolski@gmail.com>
2019-02-19 23:21:39 +0100

    [T-26] Added braces to velocity component

--- source/ecs/component/Velocity.cpp
+++ source/ecs/component/Velocity.cpp
@@ -7,47 +7,47 @@ Velocity::Velocity(float _x, float _y )
 
 Velocity Velocity::operator+( const Velocity & second )
 {
-	return Velocity( x + second.x, y + second.y );
+	return { x + second.x, y + second.y };
 }
 
 Velocity Velocity::operator-( const Velocity & second )
 {
-	return Velocity( x - second.x, y - second.y );
+	return { x - second.x, y - second.y };
 }
 
 Velocity Velocity::operator*( const Velocity & second )
 {
-	return Velocity( x * second.x, y * second.y );
+	return { x * second.x, y * second.y };
 }
 
 Velocity Velocity::operator/( const Velocity & second )
 {
-	return Velocity( x / second.x, y / second.y );
+	return { x / second.x, y / second.y };
 }
 
 Velocity Velocity::operator+( const float & second )
 {
-	return Velocity( x + second, y + second );
+	return { x + second, y + second };
 }
 
 Velocity Velocity::operator-( const float & second )
 {
-	return Velocity( x - second, y - second );
+	return { x - second, y - second };
 }
 
 Velocity Velocity::operator*( const float & second )
 {
-	return Velocity( x * second, y * second );
+	return { x * second, y * second };
 }
 
 Velocity Velocity::operator/( const float & second )
 {
-	return Velocity( x / second, y / second );
+	return { x / second, y / second };
 }
 
 float Velocity::getValue()
 {
-	return sqrt( pow( x, 2 ) + pow( y, 2 ) );
+	return static_cast<float>(sqrt( pow( x, 2 ) + pow( y, 2 ) ));
 }
 
 sf::Vector2f Velocity::getAsVector()
